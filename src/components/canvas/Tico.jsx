f31f9b01afc10012e56046b5c8ecef86cd6d9869
MANVITH14 <manvithmanu225@gmail.com>
2026-02-15 14:16:18 +0530

    Testing cube instead of GLTF

--- src/components/canvas/Tico.jsx
+++ src/components/canvas/Tico.jsx
@@ -1,13 +1,14 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Preload } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Ticofab = () => {
   return (
     <mesh>
-      <hemisphereLight intensity={0.4} groundColor="black" />
-      <pointLight intensity={0.8} />
+      <hemisphereLight intensity={0.3} groundColor="black" />
+      <pointLight intensity={1} />
+
       <boxGeometry args={[2, 2, 2]} />
       <meshStandardMaterial color="orange" />
     </mesh>
@@ -15,24 +16,40 @@ const Ticofab = () => {
 };
 
 const TicofabCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       shadows={false}
       dpr={1}
-      camera={{ position: [4, 4, 4], fov: 45 }}
-      gl={{
-        antialias: false,
-        powerPreference: "low-power"
-      }}
+      camera={{ position: [5, 5, 5], fov: 45 }}
+      gl={{ antialias: false }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
+          autoRotateSpeed={1.5}
           enableZoom={false}
-          enableRotate={true}
-          autoRotate={false}
         />
-        <Ticofab />
+        <Ticofab isMobile={isMobile} />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   );
 };
